bc511d5d37d8a55e47cf95073588422400bdcff6
Student <example@example.com>
2018-09-20 14:20:11 -0700

    finished delete function with redux

--- src/actions/items.js
+++ src/actions/items.js
@@ -1,13 +1,13 @@
-export function itemHasErrored(bool) {
+export function itemsHasErrored(bool) {
     return {
-        type: 'ITEM_HAS_ERRORED',
+        type: 'ITEMS_HAS_ERRORED',
         hasErrored: bool
     };
 }
 
-export function itemIsLoading(bool) {
+export function itemsIsLoading(bool) {
     return {
-        type: 'ITEM_IS_LOADING',    
+        type: 'ITEMS_IS_LOADING',    
         isLoading: bool
     }
 }
@@ -21,19 +21,26 @@ export function itemsFetchDataSuccess(items) {
 
 export function itemsFetchData(url) {
     return (dispatch) => {
-        dispatch(itemIsLoading(true));
+        dispatch(itemsIsLoading(true));
 
         fetch(url)
         .then((response) => {
             if(!response.ok) {
             throw Error(response.statusText);
         }
-        dispatch(itemIsLoading(false)); 
+        dispatch(itemsIsLoading(false)); 
         return response;
     })
     .then((response) => response.json())
     .then((items) => dispatch(itemsFetchDataSuccess(items)))
-    .catch(() => dispatch(itemHasErrored(true)));
+    .catch(() => dispatch(itemsHasErrored(true)));
 
     }    
+}
+
+export function deleteItem(index) {
+    return{
+        type: 'DELETE_ITEM',
+        index
+    }
 }
